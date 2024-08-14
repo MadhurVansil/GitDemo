318f38bc8d80e08d5988652cfa3b43d7945e9956
Madhur Vansil <madhurvansil@gmail.com>
2024-08-14 16:45:09 +0530

    commit 3rd time

--- src/test/java/rahulshettyacademy/tests/TestGit.java
+++ src/test/java/rahulshettyacademy/tests/TestGit.java
@@ -2,6 +2,8 @@ package rahulshettyacademy.tests;
 
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class TestGit {
 
 	
@@ -11,11 +13,18 @@ public class TestGit {
 		
 		System.out.println("This code will get commited on GIT");
 		System.out.println("commiting code second time");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
 	}
 	
 	public void cod_commit2()
 	{
 		
 		System.out.println("This code is committed second time while x person does not have this code in this machine");
+		
+		System.out.println("should be commited by gitx QA");
 	}
 }
