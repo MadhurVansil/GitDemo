5c49402212414ddc86685d4a17e279d2788e9585
Madhur Vansil <madhurvansil@gmail.com>
2024-08-14 14:03:35 +0530

    this is commit from Asian Timezone guy

---
+++ src/test/java/rahulshettyacademy/tests/TestGit.java
@@ -0,0 +1,14 @@
+package rahulshettyacademy.tests;
+
+import org.testng.annotations.Test;
+
+public class TestGit {
+
+	
+	@Test
+	public void code_Commit1()
+	{
+		
+		System.out.println("This code will get commited on GIT");
+	}
+}
